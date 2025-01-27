c8c491b6287e7ec3a9cb76951832f02b616d672d
ianjali <amudgal26@gwmail.gwu.edu>
2025-01-28 03:58:26 +0530

    clean up

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-# import os
-# import torch
-# from datasets import load_dataset
-# from transformers import (
-#     AutoModelForCausalLM,
-#     AutoTokenizer,
-#     BitsAndBytesConfig,
-#     HfArgumentParser,
-#     TrainingArguments,
-#     pipeline,
-#     logging,
-# )
 from src.pipelines.stage_1_data_pipeline import DataIngestionTrainingPipeline
 from src.pipelines.stage_2_model_training import ModelTrainerPipeline
-# from peft import LoraConfig, PeftModel
-# from trl import SFTTrainer
 
 STAGE_1="Data Preparation"
 data_ingestion_pipeline = DataIngestionTrainingPipeline()
